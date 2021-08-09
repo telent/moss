b80c6880769f63ca6a7226360fe1ea1db5e08bcd
Daniel Barlow <dan@telent.net>
2021-08-09 18:01:05 +0100

    upgrade to age 1.0.0-rc3 for encrypted key files

--- shell.nix
+++ shell.nix
@@ -4,9 +4,21 @@ let gems = bundlerEnv {
       groups = [ "default" "production" "development" "test" ];
       gemdir = ./.;
     };
+    age = pkgs.buildGoModule rec {
+      pname = "age";
+      version = "1.0.0-rc.3";
+      vendorSha256 = "sha256-sXUbfxhPmJXO+KgV/dmWmsyV49Pb6CoJLbt50yVgEvI=";
+      
+      src = fetchFromGitHub {
+        owner = "FiloSottile";
+        repo = "age";
+        rev = "v${version}";
+        sha256 = "sha256-YXdCTK9/eMvcHWg7gQQiPlLWYx2OjbOJDDNdSYO09HU=";
+      };
+    };
 in mkShell {
   packages = [  gems gems.wrappedRuby
-    bundix pkgs.age pkgs.entr ];
+    bundix age pkgs.entr ];
   shellHook = ''
     run_watch() {
       find . -type f | entr cucumber --publish-quiet -i;
