ff584153397f384d46cffe12644e2fb2c304dad4
Daniel Barlow <dan@telent.net>
2021-08-13 16:18:23 +0100

    remove cucumber ad banner noise

--- shell.nix
+++ shell.nix
@@ -7,6 +7,7 @@ let gems = bundlerEnv {
     age = pkgs.callPackage ./age.nix {};
 in mkShell {
   MOSS_HOME = "/tmp/${builtins.getEnv "USER"}/moss_home";
+  CUCUMBER_PUBLISH_QUIET = "true";
   packages = [
     gems gems.wrappedRuby
     pkgs.expect
