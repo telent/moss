c697fa517c2d1daf2752b3f369731ee67212cd0a
Daniel Barlow <dan@telent.net>
2021-08-13 16:54:06 +0100

    add ruby as a buildInput
    oops, how did I forget this

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 { stdenv
 , git
+, ruby
 , callPackage}:
 let age =  callPackage ./age.nix {};
 in stdenv.mkDerivation {
   name = "moss";
   src = ./.;
-  buildInputs = [ age git ];
+  buildInputs = [ age git ruby ];
   installPhase = ''
     make install prefix=$out
   '';
